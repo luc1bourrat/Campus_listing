06b11314ff7f5ec8ad55dc1d52ac9c5c65442f6b
Luc Bourrat <luc1.bourrat@epitech.eu>
2025-02-26 21:05:22 +0100

    ajout de la fonctionnalité pour copier les mails des DPR en un seul clique

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Tableau d'emails statique fourni
+const emailsStatic = [
+    "florian.dania@epitech.digital",
+    "valerian1.vermeulen@epitech.eu",
+    "alexandre1.caussade@epitech.eu",
+    "enzo1.scaduto@epitech.eu",
+    "guillaume1.cristiani@epitech.eu",
+    "amine.bachri@epitech.eu",
+    "lucas1.brignon@epitech.eu",
+    "pierrick.guyard@epitech.eu",
+    "francois1.gherabi@epitech.eu",
+    "lucile.casenove@epitech.eu",
+    "romain.terrier@epitech.eu",
+    "baptiste1.heraud@epitech.eu",
+    "daniel.braun@epitech.eu"
+];
+
 // Fonction pour activer/désactiver la sélection des capsules et sauvegarder l'état
 function toggleCampus(element) {
     element.classList.toggle('selected');
@@ -35,3 +52,18 @@ function resetChecks() {
 
 // Charger l'état des campus au démarrage de la page
 document.addEventListener('DOMContentLoaded', loadCampusState);
+
+// Fonction pour copier la liste des emails statiques dans le presse-papier
+function copyEmails() {
+    // Concatène les emails avec des points-virgules
+    const emailList = emailsStatic.join("; ");
+    
+    // Copie le résultat dans le presse-papier
+    navigator.clipboard.writeText(emailList)
+        .then(() => {
+            alert("Liste d'emails copiée dans le presse-papier :\n" + emailList);
+        })
+        .catch(err => {
+            console.error("Erreur lors de la copie : ", err);
+        });
+}
